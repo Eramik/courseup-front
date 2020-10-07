1d5c2e3bd18c824cd722fcb85685eee1017bb7d4
Denis <1denrocks1@gmail.com>
2020-10-07 21:24:02 +0300

    added initial project structure

---
+++ src/containers/Mainpage/Mainpage.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import styles from './Mainpage.module.scss';
+
+export class Mainpage extends Component {
+    render() {
+        return (
+            <main className={styles.Mainpage}>
+                Mainpage content
+            </main>
+        );
+    }
+}
+
+export default Mainpage
