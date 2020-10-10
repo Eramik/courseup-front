bf822e4cc6ba0eef02486ef09719c44e77a8fff3
tkachenko <dtkachenko34@gmail.com>
2020-10-10 15:05:33 +0300

    butn

--- src/components/Header/HeaderImg/HeaderImg.jsx
+++ src/components/Header/HeaderImg/HeaderImg.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import styles from './HeaderImg.module.scss'; 
+import styles from './HeaderImg.module.scss';
 
 const HeaderImg = (props) => {
     return (
-        <img src="...../public/img/HeaderImg.png" alt="logggrgem" className={styles.HeaderImg}></img>
+        <img src="../../../public/img/HeaderImg.png" alt="logggrgem" className={styles.HeaderImg}></img>
     );
 }
 
--- src/containers/StartingPage/StartingPage.jsx
+++ src/containers/StartingPage/StartingPage.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import styles from './StartingPage.module.scss';
-// import Button from '.../components/UI/Button/Button';
+import Button from '../../components/UI/Button/Button';
 
 export class StartingPage extends Component {
     render() {
         return (
             <main className={styles.StartingPage}>
-                Starting pagee
-               
+                Starting pageee
+                <Button />
             </main>
         );
     }
