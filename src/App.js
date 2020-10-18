6b1c9dfed8537b808703686d9dbeb4b09d07923d
Denis <1denrocks1@gmail.com>
2020-10-18 18:42:46 +0300

    fixed bug with course page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import StartingPage from './containers/StartingPage/StartingPage';
 import Mainpage from './containers/Mainpage/Mainpage';
-import CoursePage from './containers/CoursePage/CoursePage';
 import { Route, Switch } from 'react-router-dom';
 
 class App extends Component {
@@ -14,7 +13,6 @@ class App extends Component {
                 <Switch>
                     <Route path="/" exact component={StartingPage} />
                     <Route path="/courses" exact component={Mainpage} />
-                    <Route path="/courses/:courseId" component={CoursePage} />
                 </Switch>
                 <Footer />
             </div>
