d7f944a713da933fdc7714df460989b1bb29d487
Denis <1denrocks1@gmail.com>
2020-11-22 19:13:37 +0100

    fixed bug with review

--- src/containers/ReviewPage/ReviewPage.jsx
+++ src/containers/ReviewPage/ReviewPage.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import List from '../../components/UI/List/List.jsx';
 import Form from '../../components/UI/Form/Form.jsx';
 import '../../css/style.min.css';
+import { connect } from 'react-redux';
 
 
 class ReviewPage extends Component {
@@ -15,16 +16,15 @@ class ReviewPage extends Component {
                     rating: 3,
                     name: 'Test Name',
                     review: 'Test review Test review Test review Test reviewTest review Test review Test review Test review Test review Test review',
-                    date: new Date(new Date().setDate(new Date().getDate() - 10))
                 },
                 {
                     rating: 4,
                     name: 'Test Name',
                     review: 'Test review Test review Test review Test reviewTest reviewTest reviewTest reviewTest reviewTest reviewTest review',
-                    date: new Date() 
                 }
             ],
-            validation: ''
+            courseId: null,
+            update: 0,
         };
 
         this.submitForm = this.submitForm.bind(this);
@@ -37,33 +37,46 @@ class ReviewPage extends Component {
         });
     }
 
+    componentDidMount() {
+        const { courseId } = this.props.match.params;
+
+        fetch(`http://localhost:4000/api/v1/reviews?courseId=${courseId}`)
+            .then((results) => results.json())
+            .then((response) => {
+                const reviews = response.data.docs;
+
+                reviews.forEach((review) => {
+                    review.name = review.userId.username;
+                });
+
+                console.log(reviews);
+
+                this.setState({ reviews, courseId });      
+            })
+            .catch((e) => {
+                console.log(e.message);
+            })
+    }
+
     render()
     {
         return (
             <div className="bg-light-gray global-padding-bottom">
                 <section className="reviews">
-
-                        <div className="area align-center text-center row">
-                            <h1 className="small-12 medium-6 columns">
-                                <span className="font-weight-regular"></span><br />
-                                <span className="font-size-xxlarge text-uppercase"></span>
-                            </h1>
-                        </div>
-                   
-
+                    <div className="area align-center text-center row">
+                        <h1 className="small-12 medium-6 columns">
+                            <span className="font-weight-regular"></span><br />
+                            <span className="font-size-xxlarge text-uppercase"></span>
+                        </h1>
+                    </div>
                     <div className="row align-center content-margin-top-negative">
                         <div className="small-12 medium-8 large-6 columns">
                             <div className="content-padding bg-white area">
-
-                             
-
                                 {this.renderList()}
                             </div>
                             {this.renderForm()}
                         </div>
                     </div>
-
-
                 </section>
             </div>
 
@@ -80,10 +93,10 @@ class ReviewPage extends Component {
         return <Form submitForm={this.submitForm} validation={this.state.validation}/>;
     }
 
-    submitForm(event)
+    submitForm = async(event) =>
     {
         event.preventDefault();
-        const reviews = this.state.reviews.slice();
+        console.log(event.target.rating.value, event.target.name.value, event.target.review.value); 
 
         if(event.target.rating.value === '' || event.target.name.value === '' || event.target.review.value === '') {
             this.setState({
@@ -94,24 +107,30 @@ class ReviewPage extends Component {
             return;
         }
 
-        this.setState({
-            ...this.state,
-            validation: ''
-        });
+        const data = {
+            userId: this.props.userData._id,
+            courseId: this.state.courseId,
+            review: event.target.name.value,
+            rating: event.target.rating.value
+        };
 
-        reviews.push({
-            rating: parseInt(event.target.rating.value),
-            name: event.target.name.value,
-            review: event.target.review.value,
-            date: new Date()
-        });
+        try {
+            const response = await fetch('http://localhost:4000/api/v1/reviews/leave-review', {
+                method: 'post',
+                headers: {
+                    Authorization: `Bearer ${this.props.token}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data),
+            });
 
-        this.setState({
-            ...this.state,
-            reviews: reviews,
-            averageRating: this.getAverageRating(reviews),
-            validation: ''
-        });
+            this.setState({ update: this.state.update + 1 });
+
+            console.log(response);
+        } catch (e) {
+            console.log(e.message);
+        }
+
     }
 
     getAverageRating(reviews)
@@ -127,4 +146,11 @@ class ReviewPage extends Component {
     }
 }
 
-export default ReviewPage;
+const mapStateToProps = (state) => {
+    return {
+        token: state.token,
+        userData: state.userData
+    }
+};
+
+export default connect(mapStateToProps)(ReviewPage);
